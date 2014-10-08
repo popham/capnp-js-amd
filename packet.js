451196d4a22e616d05f32b91a795475890e50f73
Tim Popham <popham@uw.edu>
2014-10-08 16:09:19 -0700

    Update

--- packet.js
+++ packet.js
@@ -1,15 +1,23 @@
-define([ "../reader/Arena" ], function(Arena) {
+define([ "./reader/Arena", "./builder/Allocator", "./builder/copy/deep" ], function(Arena, Allocator, deep) {
+    var allocator = new Allocator();
     var fromStruct = function(instance) {
         var arena = instance._arena;
+        var singleton;
         if (arena._segments.length !== 1) {
-            throw new Error("Need to implement nontrivial serialization (predicated on size within a single segment)");
+            // Compute upper bound on necessary arena size:
+            // * Single hop far pointer implies 8 bytes of slop.
+            // * Double hop far pointer implies 16 bytes of slop.
+            var size = 0;
+            arena._segments.forEach(function(s) {
+                size += s._position;
+            });
+            var packetArena = allocator.createArena(size);
+            deep.setStructurePointer(arena, instance._layout(), packetArena, packetArena._root());
+            singleton = packetArena.getSegment(0);
+        } else {
+            singleton = arena.getSegment(0);
         }
-        var segment = arena._segments[0];
-        if (segment.length === segment._position) return [ segment ];
-        var s = segment.subarray(0, segment._position);
-        s._id = 0;
-        s._position = segment._position;
-        return s;
+        return singleton.subarray(0, singleton._position);
     };
     var toArena = function(blob) {
         return new Arena([ blob ]);
