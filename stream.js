2d79beb1e275c0f9060472831fcb18bf9f6a800d
Tim Popham <popham@uw.edu>
2014-10-19 19:08:37 -0700

    Update

--- stream.js
+++ stream.js
@@ -38,12 +38,12 @@ define([ "./reader/Arena", "./reader/primitives", "./builder/primitives" ], func
             throw new RangeError();
         }
         var header = headerPlusSegments[0];
-        for (var i = 1; i < headersPlusSegments.length; ++i) {
-            var segment = headersPlusSegments[i];
+        for (var i = 1; i < headerPlusSegments.length; ++i) {
+            var segment = headerPlusSegments[i];
             segment[i]._id = i - 1;
             segment[i]._position = segment.length;
         }
-        return new Arena(headersPlusSegments.slice(1));
+        return new Arena(headerPlusSegments.slice(1));
     };
     return {
         fromStruct: fromStruct,
